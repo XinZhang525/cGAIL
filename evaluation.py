e118fb8017946dbf6251e560af81b854fa6d5fff
XinZhang525 <xzhng160@illinois.edu>
2020-06-28 19:21:40 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,6 @@ import numpy as np
 def cross_entropy(target, ground_truth): # actually the KL-divergence
     epsilon = 1e-12
     ce = 0.
-    ce2 = 0.
     target = target.copy()
     ground_truth = ground_truth.copy()
     ces = []
@@ -12,9 +11,7 @@ def cross_entropy(target, ground_truth): # actually the KL-divergence
         target_prime = np.clip(target[state], epsilon, 1.-epsilon)
         ground_truth[state] = np.clip(ground_truth[state], epsilon, 1.-epsilon)
         t = np.sum(ground_truth[state]*np.log((target_prime/ground_truth[state])))
-        t2 = np.sum(ground_truth[state]*np.log(target_prime))
-        ce2 -= t2
         ce -= t
         ces.append(t)
-        ce2s.append(t2)
-    return ce/(len(target)), ce2/(len(target)), ces
+        
+    return ce/(len(target)), ces
