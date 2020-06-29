f4afea2a2e74eb4c94a1b25f8748c1833e064a43
XinZhang525 <xzhng160@illinois.edu>
2020-06-28 22:48:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ from a2c_ppo_acktr.algo import cgail
 from a2c_ppo_acktr.arguments import get_args
 from a2c_ppo_acktr.model import Policy
 from a2c_ppo_acktr.storage import RolloutStorage
-from a2c_ppo_acktr.envs import decide_next_state
-from a2c_ppo_acktr.envs import make_vec_envs
+from a2c_ppo_acktr.envs import * 
 from evaluation import cross_entropy
 import torch.utils.data as data_utils
 
@@ -28,12 +27,10 @@ def main():
     args = get_args()
 
     torch.manual_seed(args.seed)
-
+    
     torch.set_num_threads(1)
     device = torch.device("cuda:0" if args.cuda else "cpu")
     
-    envs = make_vec_envs(args.seed, args.num_processes,
-                         args.gamma, device, False)
     
     actor_critic = Policy(STATE_DIM, ACTION_DIM, USER_DIM)
     actor_critic.to(device)
@@ -44,15 +41,10 @@ def main():
     if args.cgail:
         discr = cgail.Discriminator(
             STATE_DIM, ACTION_DIM, USER_DIM, device, lr=args.D_lr)
-    else:
-        discr = gail.Discriminator(
-            STATE_DIM, ACTION_DIM,
-            device, lr=args.D_lr)
         
-    train_file_name = os.path.join(
-        args.experts_dir, "expert_traj.pkl")
-    test_file_name = os.path.join(
-        args.experts_dir, "test_traj.pkl")
+    train_file_name = os.path.join(args.experts_dir, "expert_traj.pkl")
+    test_file_name = os.path.join(args.experts_dir, "test_traj.pkl")
+    ground_file_name = os.path.join(args.experts_dir, "exp_loc.pkl")
     
     expert_st, expert_ur, expert_ac = pickle.load(open(train_file_name, 'rb'))
     train_load = data_utils.TensorDataset(torch.from_numpy(np.asarray(expert_st)), 
@@ -65,13 +57,15 @@ def main():
                                           torch.from_numpy(np.asarray(test_ur)), 
                                           torch.from_numpy(np.asarray(test_ac))) 
     test_loader = torch.utils.data.DataLoader(test_load, batch_size=args.gail_batch_size, shuffle=True)
+    exp_loc = pickle.load(open(ground_file_name, 'rb'))
 
+    envs = make_vec_envs(expert_st, expert_ur, args.seed, args.num_processes, args.gamma, device)
 
+    rollouts = RolloutStorage(args.num_steps, args.num_processes, STATE_DIM*5, USER_DIM, ACTION_DIM)
 
-    rollouts = RolloutStorage(args.num_steps, args.num_processes, STATE_DIM*5, ACTION_DIM)
-
-    obs = envs.reset()
-    rollouts.obs[0].copy_(obs)
+    obs, user = envs.reset()
+    rollouts.obs[0].copy_(obs[0])
+    rollouts.user[0].copy_(user[0])
     rollouts.to(device)
 
     result_log = []
@@ -94,13 +88,13 @@ def main():
 
             # Obser reward and next obs
             if action.item() != 9:
-                obs, reward, done, infos = decide_next_state(action)
-                rollouts.insert(obs, user, action, action_log_prob, value)
+                obs = decide_next_state(action, rollouts.obs[step][0], 1)
+                if obs != None:
+                    rollouts.insert(obs, rollouts.user[step], action, action_log_prob, value)
 
                 with torch.no_grad():
                     next_value = actor_critic.get_value(
                         rollouts.obs[-1], rollouts.user[-1]).detach()
-
         gail_epoch = args.gail_epoch
         if j < 10:
             gail_epoch = 100  # Warm up
@@ -125,36 +119,30 @@ def main():
         # save for every interval-th episode or for the last epoch
         if (j % args.save_interval == 0
                 or j == num_updates - 1) and args.save_dir != "":
-            save_path = os.path.join(args.save_dir, args.algo, str(args.lr), str(args.gail_batch_size), "entropy_" + str(args.entropy_coef), "D_lr"+str(args.D_lr))
+            save_path = os.path.join(args.save_dir, str(args.lr), str(args.gail_batch_size), "entropy_" + str(args.entropy_coef), "D_lr"+str(args.D_lr))
             try:
                 os.makedirs(save_path)
             except OSError:
                 pass
 
-            torch.save(actor_critic, os.path.join(save_path, args.env_name + "_ac_{}.pt".format(j)))
-            torch.save(discr, os.path.join(save_path, args.env_name + "_D_{}.pt".format(j)))
-            pickle.dump(result_log, open(os.path.join(save_path, args.env_name + "_result_log.pkl"), 'wb'))
+            torch.save(actor_critic, os.path.join(save_path,"ac_{}.pt".format(j)))
+            torch.save(discr, os.path.join(save_path, "D_{}.pt".format(j)))
 
         if j % args.log_interval == 0:
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
             end = time.time()
             print(
-                "Updates {}, num timesteps {}, FPS {} \n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n"
+                "Updates {}, num timesteps {}, FPS {}"
                 .format(j, total_num_steps,
-                        int(total_num_steps / (end - start)),
-                        len(episode_rewards), np.mean(episode_rewards),
-                        np.median(episode_rewards), np.min(episode_rewards),
-                        np.max(episode_rewards), dist_entropy, value_loss,
-                        action_loss))
+                        int(total_num_steps / (end - start))))
             
-            result_log.append([np.mean(episode_rewards), np.median(episode_rewards), np.min(episode_rewards), np.max(episode_rewards)])
             out_loc = {}
             for i, data in enumerate(test_loader, 0):
                 inputs, user, labels = data
                 inputs = inputs.float()
                 user = user.float()
                 labels = labels.long()
-                output = actor_critic.act(inputs, user).tolist()
+                output = actor_critic.act(inputs, user)[1].tolist()
 
                 for i in range(inputs.size(0)):
                     x = int(inputs[i][0].item())
@@ -177,10 +165,8 @@ def main():
                     o2 /= sum(o2)
                     target.append(o1)
                     ground.append(o2)
-            k, c, kls = cross_entropy(target, ground)
-            print(k, c)
-            diff = difference(target, ground)
-
-
+            k, kls = cross_entropy(target, ground)
+            print(k)
+            
 if __name__ == "__main__":
     main()
